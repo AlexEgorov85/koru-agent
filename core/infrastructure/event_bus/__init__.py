b808ae6f2c7d4aa2151e0f7cf79540b5baf73d82
Aleksey Egorov <alekseyvegorov@gmail.com>
2026-03-02 13:06:58 +0300

    refactor(event_bus): обновить __init__.py с экспортом новых классов

--- core/infrastructure/event_bus/__init__.py
+++ core/infrastructure/event_bus/__init__.py
@@ -2,11 +2,18 @@
 Инициализация модуля событий.
 
 АРХИТЕКТУРА:
-- event_bus: базовый класс EventBus
-- domain_event_bus: менеджер доменных шин (рекомендуется)
-- event_handlers: обработчики событий
+- event_bus: базовый класс EventBus (legacy)
+- event_bus_concurrent: конкурентная шина с сессиями (legacy)
+- domain_event_bus: менеджер доменных шин (legacy)
+- unified_event_bus: единая шина с session isolation + domain routing (рекомендуется)
+- event_bus_adapter: адаптер для обратной совместимости (временный)
 """
 from .event_bus import EventBus, EventType, Event
+from .event_bus_concurrent import (
+    EventBus as EventBusConcurrent,
+    get_event_bus as get_concurrent_event_bus,
+    create_event_bus as create_concurrent_event_bus,
+)
 from .domain_event_bus import (
     EventBusManager,
     DomainEventBus,
@@ -14,27 +21,64 @@ from .domain_event_bus import (
     EventDomain,
     EVENT_TYPE_TO_DOMAIN,
     get_event_bus_manager,
-    get_event_bus,
+    get_event_bus as get_legacy_event_bus,
     reset_event_bus_manager,
+)
+from .unified_event_bus import (
+    UnifiedEventBus,
+    Event as UnifiedEvent,
+    EventType as UnifiedEventType,
+    EventDomain as UnifiedEventDomain,
+    get_event_domain,
+    get_event_bus,
+    create_event_bus,
+    shutdown_event_bus,
+)
+from .event_bus_adapter import (
+    EventBusAdapter,
+    DomainEventBusProxy,
+    get_event_bus_adapter,
+    reset_event_bus_adapter,
 )
 from .event_handlers import MetricsEventHandler, AuditEventHandler, DebuggingEventHandler
 
 __all__ = [
-    # Базовые классы
+    # Базовые классы (legacy)
     'EventBus',
     'EventType',
     'Event',
 
-    # Доменные шины (рекомендуется)
+    # Конкурентная шина (legacy)
+    'EventBusConcurrent',
+    'get_concurrent_event_bus',
+    'create_concurrent_event_bus',
+
+    # Доменные шины (legacy, рекомендуется миграция на UnifiedEventBus)
     'EventBusManager',
     'DomainEventBus',
     'DomainEvent',
     'EventDomain',
     'EVENT_TYPE_TO_DOMAIN',
     'get_event_bus_manager',
-    'get_event_bus',
+    'get_legacy_event_bus',
     'reset_event_bus_manager',
-    
+
+    # === НОВАЯ ЕДИНАЯ ШИНА (рекомендуется) ===
+    'UnifiedEventBus',
+    'UnifiedEvent',
+    'UnifiedEventType',
+    'UnifiedEventDomain',
+    'get_event_domain',
+    'get_event_bus',  # Новый singleton
+    'create_event_bus',
+    'shutdown_event_bus',
+
+    # Адаптер для обратной совместимости (временный)
+    'EventBusAdapter',
+    'DomainEventBusProxy',
+    'get_event_bus_adapter',
+    'reset_event_bus_adapter',
+
     # Обработчики
     'MetricsEventHandler',
     'AuditEventHandler',
