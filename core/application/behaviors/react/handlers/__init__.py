72893ef0f443a5f0b0cdbc7141e24782e5731637
Aleksey Egorov <alekseyvegorov@gmail.com>
2026-03-24 13:58:19 +0300

    refactor: разделение ReActPattern на модули handlers
    - Добавлены handlers/ для ReAct паттерна:
      - handlers/base_handler.py: базовый класс
      - handlers/analyze_context_handler.py: анализ контекста
      - handlers/generate_decision_handler.py: генерация решения
    - Создан handlers/__init__.py с экспортами

---
+++ core/application/behaviors/react/handlers/__init__.py
@@ -0,0 +1,9 @@
+from .base_handler import BaseReActHandler
+from .analyze_context_handler import AnalyzeContextHandler
+from .generate_decision_handler import GenerateDecisionHandler
+
+__all__ = [
+    "BaseReActHandler",
+    "AnalyzeContextHandler",
+    "GenerateDecisionHandler",
+]
