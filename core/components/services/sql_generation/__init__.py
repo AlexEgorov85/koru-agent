32a2c26d91c67020dd32ca9d304732ab464c9508
Aleksey Egorov <alekseyvegorov@gmail.com>
2026-04-25 18:07:59 +0300

    fix: исправление не правильного импорта

--- core/components/services/sql_generation/__init__.py
+++ core/components/services/sql_generation/__init__.py
@@ -8,7 +8,7 @@ SQL Generation Service - генерация и коррекция SQL запро
 """
 from .service import SQLGenerationService
 from .error_analyzer import SQLErrorAnalyzer, ExecutionError
-from .correction import SQLCorrectionService as SQLCorrectionEngine
+from .correction import SQLCorrectionEngine
 
 __all__ = [
     "SQLGenerationService",
